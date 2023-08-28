a25e185a364f4359a9dd2f0c2b77fb2f5876678b
brage-andreas <draaangooo@gmail.com>
2023-08-29 00:38:26 +0200

    feat: reorder elements and add note counter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import NoteForm from "@/components/NoteForm";
 import Note from "@/components/note.tsx";
 
 const getNotes = async () => {
-	const res = await fetch("http://localhost:3000//api/notes/", {
+	const res = await fetch("http://localhost:3000/api/notes/", {
 		headers: { "Content-Type": "application/json" },
 		method: "GET"
 	});
@@ -16,14 +16,20 @@ export default async function Home() {
 	const data = await getNotes();
 
 	return (
-		<main className="m-34 min-w-full min-h-screen">
-			<div className="flex flex-col items-center justify-between p-24">
-				<div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-5 lg:text-left">
-					{data.map((noteData) => Note(noteData))}
+		<main className="min-w-full min-h-[80vh]">
+			<NoteForm />
+
+			<div>
+				<h2 className="uppercase font-bold">Your notes</h2>
+				<p className="text-sm mb-[1rem] text-inherit/50">
+					{data.length} note{data.length === 1 ? "" : "s"}
+				</p>
+				<div className="flex flex-col items-center justify-between mt-26">
+					<div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+						{data.map((noteData) => Note(noteData))}
+					</div>
 				</div>
 			</div>
-
-			<NoteForm />
 		</main>
 	);
 }
