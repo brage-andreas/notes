f2d9611eb3cf17879444a91b5a35e36ab51cd382
brage-andreas <draaangooo@gmail.com>
2023-08-29 00:37:56 +0200

    feat: extend layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import { type Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Lexend, Noto_Serif } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const lexend = Lexend({ subsets: ["latin"] });
+const noto = Noto_Serif({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "Notes",
@@ -15,8 +16,36 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="en">
-			<body className={inter.className}>{children}</body>
+		<html lang="en" className={lexend.className}>
+			<body className="mx-[30vw] my-[2.5vh]">
+				<header className={noto.className}>
+					<h1 className="font-bold text-5xl">Notes</h1>
+				</header>
+
+				{children}
+
+				<footer>
+					<div className="flex flex-col items-center justify-between mt-26">
+						<div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-2 lg:text-left">
+							<div>
+								<h3 className="font-bold">Notes</h3>
+								<p className="text-xs">
+									Made by Brage • 2023
+									<br />
+									💖 OSS on{" "}
+									<a
+										href="https://github.com/brage-andreas/notes"
+										className="text-indigo-500"
+									>
+										GitHub
+									</a>
+								</p>
+							</div>
+							<div>Click the notes to open them!</div>
+						</div>
+					</div>
+				</footer>
+			</body>
 		</html>
 	);
 }
