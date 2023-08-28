d42bc83e7e8cb23ff4853462a823be1e2f13dd7f
brage-andreas <draaangooo@gmail.com>
2023-08-29 00:39:35 +0200

    wip: note details

--- src/app/notes/[id]/page.tsx
+++ src/app/notes/[id]/page.tsx
@@ -1,15 +1,24 @@
-import NotFound from "./not-found.tsx";
+const getNote = async (id: string) => {
+	const res = await fetch(`http://localhost:3000/api/note/${id}`, {
+		headers: { "Content-Type": "application/json" },
+		method: "GET"
+	});
+
+	if (!res.ok) {
+		return null;
+	}
+
+	const { data } = await res.json();
+
+	return data as { id: string; createdAt: Date; content: string } | null;
+};
 
 export default async function Page({ params }: { params: { id: string } }) {
 	const note = await getNote(params.id);
 
-	if (!note) {
-		return NotFound();
-	}
-
 	return (
 		<div>
-			<h1>TEST</h1>
+			<h1>{note?.content || "Missing content"}</h1>
 		</div>
 	);
 }
