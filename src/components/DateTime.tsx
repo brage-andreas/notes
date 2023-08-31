826be7d183473727ae871fcc7e1646b00ae3ae19
brage-andreas <draaangooo@gmail.com>
2023-08-31 13:36:14 +0200

    feat: make text "now" for recent dates

--- src/components/DateTime.tsx
+++ src/components/DateTime.tsx
@@ -1,9 +1,16 @@
 import { formatDate } from "@/helpers/formatDate.ts";
 import ms from "ms";
 
-export default function DateTime(props: { date: Date }) {
-	const hoverText = formatDate(props.date);
-	const relativeTime = ms(Date.now() - props.date.getTime(), { long: true });
+const TWO_MINUTES_MS = 120_000;
 
-	return <span title={hoverText}>{relativeTime} ago</span>;
+export default function DateTime(props: { date: Date }) {
+	const timeDifference = Date.now() - props.date.getTime();
+	const hoverText = formatDate(props.date);
+
+	const relativeTime =
+		TWO_MINUTES_MS < timeDifference
+			? `${ms(timeDifference, { long: true })} ago`
+			: "Now";
+
+	return <span title={hoverText}>{relativeTime}</span>;
 }
