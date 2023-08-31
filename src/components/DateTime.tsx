92b04fb56d9acdb41169bbabb9fda65c42317953
brage-andreas <draaangooo@gmail.com>
2023-08-31 13:27:57 +0200

    feat: add ".. ago" behind relative time

--- src/components/DateTime.tsx
+++ src/components/DateTime.tsx
@@ -5,5 +5,5 @@ export default function DateTime(props: { date: Date }) {
 	const hoverText = formatDate(props.date);
 	const relativeTime = ms(Date.now() - props.date.getTime(), { long: true });
 
-	return <span title={hoverText}>{relativeTime}</span>;
+	return <span title={hoverText}>{relativeTime} ago</span>;
 }
